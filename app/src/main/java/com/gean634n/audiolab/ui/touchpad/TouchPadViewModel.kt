d2a5eddc7b2883567282c979c638ed3e7af43f1b
gean634n <gean634n@gmail.com>
2026-09-03 11:02:44 -0400

    feat: control volume and frequency with touchpad

--- app/src/main/java/com/gean634n/audiolab/ui/touchpad/TouchPadViewModel.kt
+++ app/src/main/java/com/gean634n/audiolab/ui/touchpad/TouchPadViewModel.kt
@@ -12,6 +12,8 @@ import kotlin.math.pow
 
 private const val MIN_FREQUENCY_HZ = 110f
 private const val MAX_FREQUENCY_HZ = 1760f
+private const val MIN_LEVEL_DB = -60f
+private const val MAX_LEVEL_DB = 0f
 
 class TouchPadViewModel(
     private val audioEngine: AudioEngine
@@ -23,9 +25,16 @@ class TouchPadViewModel(
     var frequencyHz by mutableFloatStateOf(yToFrequency(position.y))
         private set
 
+    var levelDb by mutableFloatStateOf(xToLevelDb(position.x))
+        private set
+
     fun onPositionChange(position: Offset) {
         this.position = position
+
+        levelDb = xToLevelDb(position.x)
         frequencyHz = yToFrequency(position.y)
+
+        audioEngine.setLevelDb(levelDb)
         audioEngine.setFrequencyHz(frequencyHz)
     }
 }
@@ -34,6 +43,10 @@ private fun yToFrequency(y: Float): Float {
     val normalizedY = 1f - y
 
     return MIN_FREQUENCY_HZ * (MAX_FREQUENCY_HZ / MIN_FREQUENCY_HZ).pow(normalizedY)
+}
+
+private fun xToLevelDb(x: Float): Float {
+    return MIN_LEVEL_DB + x * (MAX_LEVEL_DB - MIN_LEVEL_DB)
 }
 
 class TouchPadViewModelFactory(
